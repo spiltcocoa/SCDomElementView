812edf008d8eb0c198bb3c8c9cab7c68c0529e96
Jeff Boek <jeff@jeffboek.com>
2015-05-21 14:03:31 -0700

    Initial Commit

---
+++ SCDomElementView/SCDomElementView.h
@@ -0,0 +1,19 @@
+//
+//  SCDomElementView.h
+//  SCDomElementView
+//
+//  Created by Jeff Boek on 5/21/15.
+//  Copyright (c) 2015 spiltcocoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SCDomElementView.
+FOUNDATION_EXPORT double SCDomElementViewVersionNumber;
+
+//! Project version string for SCDomElementView.
+FOUNDATION_EXPORT const unsigned char SCDomElementViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCDomElementView/PublicHeader.h>
+
+
