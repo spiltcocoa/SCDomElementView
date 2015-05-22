431fff921c0c126f9f831208b43232542c753c7c
Jeff Boek <jeff@jeffboek.com>
2015-05-22 10:52:31 -0700

    Previews the element

--- SCDomElementView/SCDomElementView.swift
+++ SCDomElementView/SCDomElementView.swift
@@ -9,22 +9,69 @@
 import UIKit
 import WebKit
 
-public class SCDomElementView: UIView {
+public class SCDomElementView: UIView, WKNavigationDelegate {
 
-	public let scrollView: UIScrollView
-	public let webView: WKWebView
+	public let scrollView = UIScrollView()
 
-	public init(frame: CGRect, request: NSURLRequest) {
-		scrollView = UIScrollView(frame: CGRectZero)
-		webView = WKWebView()
+	private(set) lazy var webView: WKWebView = {
+		let js = "var meta = document.createElement('meta'); meta.setAttribute('name', 'viewport'); meta.setAttribute('content', 'initial-scale=1.0'); document.getElementsByTagName('head')[0].appendChild(meta);"
+		let script = WKUserScript(source: js, injectionTime: .AtDocumentEnd, forMainFrameOnly: true)
 
+		let controller = WKUserContentController()
+		controller.addUserScript(script)
+
+		let config = WKWebViewConfiguration()
+		config.userContentController = controller
+
+		return WKWebView(frame: self.frame, configuration: config)
+		}()
+
+	public override init(frame: CGRect) {
 		super.init(frame: frame)
+		configure()
 	}
 
 	public required init(coder aDecoder: NSCoder) {
-		scrollView = UIScrollView(frame: CGRectZero)
-		webView = WKWebView()
+		super.init(coder: aDecoder)
 
-	    super.init(coder: aDecoder)
+		configure()
+	}
+
+	private func configure() {
+		backgroundColor = UIColor.redColor()
+		webView.navigationDelegate = self
+//		webView.userInteractionEnabled = false
+
+		addSubview(webView)
+//		scrollView.addSubview(webView)
+	}
+
+	private func rectFromId(id: String, callback: (CGRect) -> ()) {
+		var javascript = "function f(){ var r = document.getElementById('\(id)').getBoundingClientRect(); return '{{'+r.left+','+r.top+'},{'+r.width+','+r.height+'}}'; } f();";
+
+		webView.evaluateJavaScript(javascript) { result, error in
+			if error != nil { return }
+
+			if let result = result as? String {
+				let rect = CGRectFromString(result)
+
+				callback(rect)
+			}
+		}
+	}
+
+	public func loadRequest(request: NSURLRequest) {
+		webView.loadRequest(request)
+	}
+
+	public func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+		self.rectFromId("mobile") { rect in
+			var frame = rect
+			println(frame.origin)
+			self.webView.scrollView.setContentOffset(frame.origin, animated: false)
+			println(self.webView.scrollView.contentOffset)
+			frame.origin = CGPointZero
+			self.webView.frame = frame
+		}
 	}
 }
---
+++ iOS Example/iOS Example/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  iOS Example
+//
+//  Created by Jeff Boek on 5/21/15.
+//  Copyright (c) 2015 spiltcocoa. All rights reserved.
+//
+
+import UIKit
+import SCDomElementView
+
+class ViewController: UIViewController {
+
+	@IBOutlet weak var elementView: SCDomElementView!
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		let url = "http://192.168.1.2:3000/snapshots/80611367-d48e-4136-b82c-95e6957b5703"
+		//f59274fb-55b7-45ad-853d-7e69aac3331e" //
+		if let nsUrl = NSURL(string: url) {
+			let request = NSMutableURLRequest(URL: nsUrl, cachePolicy: .ReturnCacheDataElseLoad, timeoutInterval: 30)
+			request.setValue("f28f3d351d03e881478ef0f3aae47113", forHTTPHeaderField: "OPAL-AUTH-TOKEN")
+
+			elementView.loadRequest(request)
+		}
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
